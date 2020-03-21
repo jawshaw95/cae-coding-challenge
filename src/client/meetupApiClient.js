f408d46dc4b582c14938caaf920338a4c3be6e68
Justin Shaw <jawshaw95@gmail.com>
2020-03-21 16:58:25 -0500

    add function to get all events

--- src/client/meetupApiClient.js
+++ src/client/meetupApiClient.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import event from "../mock_data/event";
 import eventRSVP from "../mock_data/eventRSVP";
 import eventsResponse from "../mock_data/eventsResponse";
 
@@ -9,16 +8,19 @@ import eventsResponse from "../mock_data/eventsResponse";
 const base = "https://cors-anywhere.herokuapp.com/https://api.meetup.com";
 
 export const fetchEvents = group => {
-  // if(process.env.NODE_ENV === 'development') {
-  //      return setTimeout(() => resolve())
-  // }
-  // axios.get(`${base}/${group}/events`);
+  if(process.env.NODE_ENV === 'development') {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve({ data: eventsResponse }), 1);
+    });
+  }
+  return axios.get(`${base}/${group}/events`);
 };
 
 export const fetchEvent = (group, eventId) => {
   if (process.env.NODE_ENV === "development") {
     return new Promise((resolve, reject) => {
-      setTimeout(() => resolve({ data: event }), 1);
+      setTimeout(() => resolve({ data: eventsResponse.filter(event => event.id ===
+        eventId)[0] }), 1);
     });
   }
   return axios.get(`${base}/${group}/events/${eventId}`);
